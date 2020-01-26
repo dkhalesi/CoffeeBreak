14536de66d2af28680a81ed031efaf80420900f5
Dina Khalesi <dinakhalesit@gmail.com>
2020-01-26 02:19:51 -0500

    added values to drop down

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+var bodyParser = require('body-parser');
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve('./public/homePage.html'));
 });
 
 router.post('/contact', function(req, res, next){
-  console.log(req);
-})
+  // if((req.body.EmployeeIds).isInteger && req.body.departments !== ""){
+
+  // }
+  console.log('!!');
+});
 
 module.exports = router;
 
